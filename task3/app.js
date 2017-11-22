9bd5ca96436e811c7dad0649754b9a10e3f4e58c
Andrew <pelpro@yandex.ru>
2017-11-22 23:03:41 +0300

    web

---
+++ task3/app.js
@@ -0,0 +1,17 @@
+function CreateandAdd(s) {
+var element = $('<li><span></span> <button class="deleteb">Удалить</button></li>');
+$('span', element).text(s);
+$('#root ul').append(element);
+$('.deleteb', element).click(function(delq){
+$(this).parent().remove()});
+}
+$(function(){
+$('#root').append('<ul></ul>');
+$('#root').append('<input id="add_task_input">)
+$('#root').append('<button id="add_task">Добавить</button>');
+$('#add_task').click(function(){
+addElement($('#add_task_input').val())
+}
+);
+CreateandAdd('Сделать задание #3 по web-программированию');
+})
